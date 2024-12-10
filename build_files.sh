158a725d18f3dcb69ac89c8adf155c8d76adec52
Muhammad Sharf Uddin <developersharf@gmail.com>
2024-12-10 12:51:48 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# Install Python and pip if not available
+apt-get update && apt-get install -y python3 python3-pip
+
+# Install dependencies
+pip install -r requirements.txt
+
+
 pip install -r requirements.txt 
 python3.9 manage.py collectstatic
 
